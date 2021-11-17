3b5e9aec8555b26c52bf49513d5b9cf3f8a3936d
chirag4206 <76816323+chirag4206@users.noreply.github.com>
2021-11-17 16:41:10 +0530

    Add files via upload

---
+++ class-98/function.py
@@ -0,0 +1,21 @@
+def myFunction(name):
+    print("hello "+name)
+myFunction("chirag")
+
+def evenOdd(n):
+    if(n%2==0):
+        print("number is even")
+    else:
+        print ("number is odd")
+
+
+import math
+def squareRoot(num):
+    print(math.sqrt(num))
+
+
+squareRoot(6)
+evenOdd(7)
+
+f=open("text.txt")
+f.read()
